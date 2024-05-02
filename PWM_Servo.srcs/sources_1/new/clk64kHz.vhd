1d00ae1585d028193196ed284f567a3e01b39f07
Karlos1235 <82879745+Karlos1235@users.noreply.github.com>
2024-05-02 10:23:44 +0200

    Update clk64kHz.vhd

--- PWM_Servo.srcs/sources_1/new/clk64kHz.vhd
+++ PWM_Servo.srcs/sources_1/new/clk64kHz.vhd
@@ -10,22 +10,22 @@ entity clk64kHz is
 end clk64kHz;
  
 architecture Behavioral of clk64kHz is
-    signal temporal: STD_LOGIC;
-    signal counter : integer range 0 to 780 := 0;
+    signal temporal: STD_LOGIC; -- Pomocný signál pro uchování stavu
+    signal counter : integer range 0 to 780 := 0; -- Čítač impulsů
 begin
-    freq_divider: process (reset, clk) begin
-        if (reset = '1') then
-            temporal <= '0';
-            counter  <= 0;
-        elsif rising_edge(clk) then
-            if (counter = 780) then
-                temporal <= NOT(temporal);
-                counter  <= 0;
+    freq_divider: process (reset, clk) begin -- Proces pro dělení frekvence
+        if (reset = '1') then -- Pokud je reset aktivní
+            temporal <= '0'; -- Nastavíme výstupní signál na nulu
+            counter  <= 0; -- Resetujeme čítač
+        elsif rising_edge(clk) then -- Na náběžné hraně
+            if (counter = 780) then -- Pokud čítač dosáhne maxima
+                temporal <= NOT(temporal); -- Invertujeme výstupní signál
+                counter  <= 0; -- Resetujeme čítač
             else
-                counter <= counter + 1;
+                counter <= counter + 1; --Jinak inkrementujeme čítač
             end if;
         end if;
     end process;
  
-    clk_out <= temporal;
+    clk_out <= temporal; -- Připojení výstupního signálu k výstupnímu portu
 end Behavioral;
