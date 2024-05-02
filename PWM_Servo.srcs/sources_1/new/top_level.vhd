ec7182ef1e84d19501c93dfa2dedcbb51b3fd29a
Karlos1235 <82879745+Karlos1235@users.noreply.github.com>
2024-05-02 10:49:51 +0200

    Update top_level.vhd

--- PWM_Servo.srcs/sources_1/new/top_level.vhd
+++ PWM_Servo.srcs/sources_1/new/top_level.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity top_level is
     Port (
-        CLK       : in  STD_LOGIC;
-        BTNC      : in  STD_LOGIC;
+        CLK       : in  STD_LOGIC; -- Clock input
+        BTNC      : in  STD_LOGIC; 
         BTND      : in  STD_LOGIC;
         BTNL      : in  STD_LOGIC;
         BTNU      : in  STD_LOGIC;
@@ -17,6 +17,7 @@ end top_level;
 
 architecture Behavioral of top_level is
 
+    -- Declaration of the servo_control component
     component servo_control
         Port (
             CLK       : in  STD_LOGIC;
@@ -31,10 +32,12 @@ architecture Behavioral of top_level is
         );
     end component;
 
+    -- Declaration of internal signals
     signal JA_internal, JB_internal, JC_internal, JD_internal: STD_LOGIC;
 
 begin
 
+    -- Instantiation of the servo_control component
     servo_inst : servo_control
         port map (
             CLK   => CLK,
